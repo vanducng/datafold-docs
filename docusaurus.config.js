8f73eac8b6e1569bbe9f151517c3da3c9330e613
Ari Bajo <2302748+arimbr@users.noreply.github.com>
2023-03-07 15:28:06 +0100

    Fix canonical links (#134)
    All docs pages seem to have the canonical link pointing to datafold.com instead of docs.datafold.com.
    
    The canonical link should be the same as the URL page.
    
    You can find the canonical url, by inspecting the HTML and checking for this element:
    `<link rel="canonical" href="https://datafold.com/" data-rh="true">`
    
    Note: I haven't tested locally that the change fixes the issue yet.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Datafold',
   tagline: 'Automated Testing for Data',
-  url: 'https://datafold.com',
+  url: 'https://docs.datafold.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
